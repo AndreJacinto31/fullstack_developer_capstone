8c4baca03dcea73795d74c7f11c921b8ae0732a6
AndreJacinto31 <andre.jacinto@hotmail.com>
2024-02-20 18:06:30 -0500

    changes made from theia lab environment

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -3,18 +3,24 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
+from .restapis import get_request, analyze_review_sentiments, post_review
 
 app_name = 'djangoapp'
 urlpatterns = [
     # path for registration
     path(route='registration', view=views.registration, name='registration'),
     # path for login
-     path(route='login', view=views.login_user, name='login'),
-     path(route='logout', view=views.registration, name='logout'),
-    # path for dealer reviews view
-    
+    path(route='login', view=views.login_user, name='login'),
+    path(route='logout', view=views.registration, name='logout'),
+    # path for dealers
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    # path for dealer details
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
     # path for add a review view
-
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    # path for dealer reviews view
+    path(route='add_review', view=views.add_review, name='add_review'),
     # path for get cars
     path(route='get_cars', view=views.get_cars, name ='getcars'),
 
